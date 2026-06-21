46f953d8f5662a3ca377bd924d0940f9ebe1227d
Nithin-Jagannathun <nithin.jagan2007@gmail.com>
2026-06-21 22:17:11 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,30 +12,46 @@ public class Main {
         while (true) {
             System.out.print("$ ");
             String command = sc.nextLine();
+            String[] parts = command.split(" ");
 
             if (command.equals("exit")) break;
             else if (command.startsWith("type ")) {
-                String cmd = command.substring(5);
-                if (cmds.contains(cmd)) {
-                    System.out.println(cmd + " is a shell builtin");
+                if (cmds.contains(parts[0])) {
+                    System.out.println(parts[0] + " is a shell builtin");
                 }
-                else {
-                    
+                else {         
                     String path = System.getenv("PATH");
                     String[] directories = path.split(":");
+                    File executable = null;
 
                     boolean found = false;
 
                     for(String directory: directories) {
-                        File file = new File(directory, cmd);
+                        File file = new File(directory, parts[0]);
                         if (file.exists() && file.canExecute()) {
                             found = true;
-                            System.out.println(cmd + " is " + file.getAbsolutePath());
+                            executable = file;
                             break;
                         }
                     }
 
-                    if (!found) System.out.println(cmd  + ": not found");
+                    if (found) {
+                        ArrayList<String> cmd = new ArrayList<>();
+                        cmd.add(executable.getAbsolutePath());
+
+                        for(int i = 1; i < parts.length; i++) {
+                            cmd.add(parts[i]);
+                        }
+
+                        ProcessBuilder pb = new ProcessBuilder(cmd);
+                        pb.inheritIO();
+
+                        Process p = pb.start();
+                        p.waitFor();
+                    } 
+                    else {
+                        System.out.println(parts[0]  + ": not found");
+                    }
                 }
             }
             else if (command.startsWith("echo ")){
